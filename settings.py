01c0ea57762cc444f4f1893163f152eb1040c90b
proosjen <proosjen@msu.edu>
2017-11-03 14:44:18 -0400

    Start preprocessing data

---
+++ settings.py
@@ -0,0 +1,8 @@
+C_KEY = 'Qiin47L4D53g3zH0GIfXG3egR'
+C_SECRET = 'QMUgcciWcCgeklW3ipyLRdJYlQgWYWQOQU4rKhrurE58UyORQg'
+A_TOKEN_KEY = '907976726097080321-WNdhYqcbztSbHviU3APVjJi9KlTgAeY'
+A_TOKEN_SECRET = 'KiwAd38Pyxe7qrrAhZUhE2W1ScZT4l2UgJ5yByCWb1U0f'
+
+keywords = ['southwest airlines', 'SouthwestAir', 'southwestairlines', 'flying southwest', 'fly southwest']
+trainTable = "trainTweets"
+testTable = "testTweets"
